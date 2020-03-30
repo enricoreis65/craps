2a917acf4310287feaf1e576bf479ad5e6e11e6f
Enrico Borges <enricolbc@al.insper.edu.br>
2020-03-30 12:39:12 -0300

    primeiro teste

--- trabalho.py
+++ trabalho.py
@@ -8,29 +8,28 @@ while fichas>0:
     status=input("voce quer jogar? ")
     if status=="s":
        if rodada=="Come_Out":
-           tipo_de_aposta=input("voce quer apostar em: Pass Line Bet , Field , Any Craps ou Twelve?")
-
-           if tipo_de_aposta=="Pass":
-               valor_da_aposta=int(input("quanto vc quer apostar? "))
-               d1=randint(1,6)
-               d2=randint(1,6)
-               soma = d1+d2
-                 if d1+d2==7 or d1+d2==11:
-                  fichas+=valor_da_aposta
-                 if d1+d2==2 or d1+d2==3 or d1+d2==12:
+            tipo_de_aposta=input("voce quer apostar em: Pass Line Bet , Field , Any Craps ou Twelve?")
+            if tipo_de_aposta=="Pass":
+                valor_da_aposta=int(input("quanto vc quer apostar? "))
+                d1=randint(1,6)
+                d2=randint(1,6)
+                soma = d1+d2
+                if soma==7 or soma==11:
+                    fichas+=valor_da_aposta
+                if soma==2 or soma==3 or soma==12:
                     fichas-=valor_da_aposta
-                if d1+d2==4 or d1+d2==5 or d1+d2==6 or d1+d2==8 or d1+d2==9 or d1+d2==10:
+                if soma==4 or soma==5 or soma==6 or soma==8 or soma==9 or soma==10:
                     rodada="Point"
                     
-            if tipo_de_aposta == 'Field':
-               if soma == 5 or soma == 6 or soma == 7 or soma == 8:
-                   valor_da_aposta = 0
-               elif soma == 3 or soma == 4 or soma == 9 or soma == 10 or soma == 11:
-                   fichas += valor_da_aposta
-               elif soma == 2:
-                   fichas += valor_da_aposta*2
-               else:
-                   fichas += valor_da_aposta*3         
+            if tipo_de_aposta == "Field":
+                if soma == 5 or soma == 6 or soma == 7 or soma == 8:
+                    valor_da_aposta = 0
+                elif soma == 3 or soma == 4 or soma == 9 or soma == 10 or soma == 11:
+                    fichas += valor_da_aposta
+                elif soma == 2:
+                    fichas += valor_da_aposta*2
+                else:
+                    fichas += valor_da_aposta*3         
 
 
 
