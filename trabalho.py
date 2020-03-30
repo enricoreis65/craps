b7b4c16362881ead4ed493b2b878cd0a990ae628
Enrico Borges <enricolbc@al.insper.edu.br>
2020-03-30 11:17:34 -0300

    programa

--- trabalho.py
+++ trabalho.py
@@ -1,1 +1,15 @@
-#trabalho iniciando
+
+fichas=100
+while fichas<0:
+    status=input("voce quer jogar? ")
+    if status=="s":
+
+
+
+
+
+
+    elif status=="n":
+        dinheiro_final=fichas
+        fichas-=fichas
+print(dinheiro_final)
