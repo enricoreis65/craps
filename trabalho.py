297a4a0ac9e405d9bf6ec355d8dad581456d48ea
annabgl <56547406+annabgl@users.noreply.github.com>
2020-03-30 11:52:27 -0300

    random

--- trabalho.py
+++ trabalho.py
@@ -1,12 +1,14 @@
+from random import randint
 dinheiro_final=0
 fichas=100
 rodada="Come_Out"
+
 
 while fichas>0:
     status=input("voce quer jogar? ")
     if status=="s":
        if rodada=="Come_Out":
-           tipo_de_aposta=input("voce quer apostar em: Pass Line Bet , Field , Any Craps ou Twelve?)
+           tipo_de_aposta=input("voce quer apostar em: Pass Line Bet , Field , Any Craps ou Twelve?")
 
     else:
         dinheiro_final=fichas
