ac064738f00f2f69c46a2cfea77b283a5487fcaf
Enrico Borges <enricolbc@al.insper.edu.br>
2020-03-30 11:44:11 -0300

    apostas

--- trabalho.py
+++ trabalho.py
@@ -1,9 +1,12 @@
 dinheiro_final=0
 fichas=100
+rodada="Come_Out"
+
 while fichas>0:
     status=input("voce quer jogar? ")
     if status=="s":
-        print(fichas)
+       if rodada=="Come_Out":
+           tipo_de_aposta=input("voce quer apostar em: Pass Line Bet , Field , Any Craps ou Twelve?)
 
     else:
         dinheiro_final=fichas
