dceb26dea49ea4fc1cf4701883b0912d7a0c1a85
Enrico Borges <enricolbc@al.insper.edu.br>
2020-03-30 12:13:22 -0300

    pass

--- trabalho.py
+++ trabalho.py
@@ -1,5 +1,5 @@
 from random import randint
-dinheiro_final=0
+
 fichas=100
 rodada="Come_Out"
 
@@ -10,10 +10,22 @@ while fichas>0:
        if rodada=="Come_Out":
            tipo_de_aposta=input("voce quer apostar em: Pass Line Bet , Field , Any Craps ou Twelve?")
 
+           if tipo_de_aposta=="Pass":
+               valor_da_aposta=int(input("quanto vc quer apostar? ")
+               d1=randint(1,6)
+               d2=randint(1,6)
+                if d1+d2==7 or d1+d2==11:
+                   fichas+=valor_da_aposta
+                if d1+d2==2 or d1+d2==3 or d1+d2==12:
+                    fichas-=valor_da_aposta
+                if d1+d2==4 or d1+d2==5 or d1+d2==6 or d1+d2==8 or d1+d2==9 or d1+d2==10:
+                    rodada="Point"
+
+
+
     else:
-        dinheiro_final=fichas
+        
         fichas-=fichas
-print(dinheiro_final)
 
 
 
